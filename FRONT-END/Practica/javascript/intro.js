e5c3130bfb4763f6b5866eb79689cc184cee8dfe
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-03-12 19:20:52 -0600

    Practice Js

---
+++ FRONT-END/Practica/javascript/intro.js
@@ -0,0 +1,19 @@
+//Condicionales IF
+let number1 = 4;
+let number2 = 10;
+
+if (number1 > number2) {
+    console.log("si se cumple aqui");
+} else if (number1 > 2 && number2 == 10) {
+    console.log("si se cumple aqui 2");
+} else {
+    console.log("si se cumple aqui 3");
+}
+
+//Ciclo While
+let numberWhile = 10;
+
+while (numberWhile <= 100) {
+    console.log(numberWhile);
+    numberWhile = numberWhile + 5;
+}
